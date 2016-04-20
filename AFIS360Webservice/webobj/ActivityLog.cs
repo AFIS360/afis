c592f93f5989d3cd78b32642514795a08e2b3b31
mmohsin4706 <mmohsin4706@yahoo.com>
2016-04-20 10:37:50 -0500

    Web Service object

---
+++ AFIS360Webservice/webobj/ActivityLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AFIS360Webservice.webobj
+{
+    public class ActivityLog
+    {
+        private List<string> activityLogs = null;
+
+        public ActivityLog()
+        {
+            activityLogs = new List<string>();
+        }
+
+        public List<string> Activity
+        {
+            get { return this.activityLogs; }
+            set { this.activityLogs = value; }
+        }
+    }
+}
